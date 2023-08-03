2ec020f902465518db5ef54deb8eb8349266ec14
soobin83 <soobin83@gmail.com>
2023-08-02 23:12:11 -0400

    two more notebooks

--- dashapp.py
+++ dashapp.py
@@ -18,6 +18,33 @@ app.layout = html.Div([
     html.H3(children='Choose parameters to see if your stock is a BUY or HOLD'),
     html.P(children='BUY or HOLD?'),
     html.Br(),
+    dcc.Dropdown(
+        ["BUY","HOLD"],
+        id='dropdown-recommendation',
+        placeholder="Choose the analyst recommemdation",
+        style= {
+            "width": "30em"
+        },
+    ),
+    html.Br(),
+    dcc.Dropdown(
+        ["Technology","Consumer Cyclical", "Communication Services", "Energy", "Healthcare", "Financial", "Consumer Defensive", "Industrials"],
+        id='dropdown-sector',
+        placeholder="Which sector are you interested in",
+        style= {
+            "width": "30em"
+        },
+    ),
+    html.Br(),
+    dcc.Dropdown(
+        ["Yes","No"],
+        id='dropdown-dividend',
+        placeholder="Does the stock payout dividend",
+        style= {
+            "width": "30em"
+        },
+    ),
+    html.Br(),
     dcc.Input(
         id='input-marketcap',
         type='number',
@@ -54,15 +81,6 @@ app.layout = html.Div([
         },
     ),
     html.Br(),
-    dcc.Dropdown(
-        ["BUY","HOLD"],
-        id='dropdown-recommendation',
-        placeholder="Choose the analyst recommemdation",
-        style= {
-            "width": "30em"
-        },
-    ),
-    html.Br(),
     html.Button('Submit', id='submit-val', n_clicks=0),
     html.Br(),
     html.Br(),
@@ -73,22 +91,33 @@ app.layout = html.Div([
 @callback(
     Output('p-result', 'children'),
     Input('submit-val', "n_clicks"),
+    State('dropdown-recommendation', 'value'),
+    State('dropdown-sector', 'value'),
+    State('dropdown-dividend', 'value'),
     State('input-marketcap', 'value'),
     State('input-sales', 'value'),
     State('input-profitmargin', 'value'),
     State('input-multiple', 'value'),
-    State('dropdown-recommendation', 'value'),
     prevent_initial_call=True
 )
-def update_result(clicks, amount_marketcap, amount_sales, 
+def update_result(clicks, recommendation, sector, dividend, amount_marketcap, amount_sales, 
                   amount_profitmargin, count_multiple,
-                  recommendation):
+                  ):
     info_for_prediction = {
+        "Recommendation": 0 if recommendation=="HOLD" else 1,
+        "Sector": 0 if sector=="Technology" else 1,
+        "Sector": 0 if recommendation=="Consumer Cyclical" else 1,
+        "Sector": 0 if recommendation=="Communication Services" else 1,
+        "Sector": 0 if recommendation=="Energy" else 1,
+        "Sector": 0 if recommendation=="Healthcare" else 1,
+        "Sector": 0 if recommendation=="Financial" else 1,
+        "Sector": 0 if recommendation=="Consumer Defensive" else 1,
+        "Sector": 0 if recommendation=="Industrials" else 1,
+        "Dividend": 0 if dividend=="No" else 1,
         "Market Cap": float(amount_marketcap),
         "Sales": float(amount_sales),
         "Profit Margin": float(amount_profitmargin),
         "Forward P/E": float(count_multiple),
-        "Recommendation": 0 if recommendation=="HOLD" else 1,
         }
     df_predict = pd.DataFrame(info_for_prediction,index=[0])
     df_predict = classifier.transform(df_predict)
